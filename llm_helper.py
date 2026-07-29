04eb436c209dd00c472dae27645d059616fcf7b4
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2026-07-30 03:41:50 +0530

    Deploy

--- llm_helper.py
+++ llm_helper.py
@@ -1,11 +1,8 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 from langchain_groq import ChatGroq
 
-load_dotenv()
-
 llm = ChatGroq(
-    api_key=os.getenv("GROQ_API_KEY"),
+    api_key=st.secrets["GROQ_API_KEY"],
     model="llama-3.3-70b-versatile",
     temperature=0.7,
 )
