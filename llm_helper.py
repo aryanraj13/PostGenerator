6069bd2b484d26352b1e5c4a282a5c4f68827eee
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2026-07-30 03:36:21 +0530

    Deploy

--- llm_helper.py
+++ llm_helper.py
@@ -1,16 +1,11 @@
-from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
 
 load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
 
-
-if __name__ == "__main__":
-    response = llm.invoke("Two most important ingradient in samosa are ")
-    print(response.content)
-
-
-
-
-
+llm = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"),
+    model="llama-3.3-70b-versatile",
+    temperature=0.7,
+)
